974ec46cd372bbc590601263cd9dd5654e166c10
Akarsh Rajput <112371948+akarshrajput@users.noreply.github.com>
2023-12-13 16:47:41 +0530

    Second Commit

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+app.use(cors());
 const expenseRouter = require("./routes/expenseRouter");
 app.use(express.json());
 app.use("/api/v1/expenses", expenseRouter);
